92d11b967d84d68f179da50ad99e230e8dbe0dcb
Marrek Nožka <marek@tlapicka.net>
2019-03-06 14:09:05 +0100

    PEPka

--- druha_verze.py
+++ druha_verze.py
@@ -13,8 +13,9 @@ from pyglet.window import key
 window = pyglet.window.Window(1000, 800)
 batch = pyglet.graphics.Batch()
 
+
 class All_objects(pyglet.sprite.Sprite):
-    
+
     def __init__(self, img_file, x=None, y=None):
         self.image_load = pyglet.image.load(img_file)
         # střed otáčení dám na střed obrázku
@@ -24,139 +25,156 @@ class All_objects(pyglet.sprite.Sprite):
         super().__init__(self.image_load,  batch=batch)
         self.x = x if x is not None else random.randint(0, window.width)
         self.y = y if y is not None else random.randint(0, window.height)
-        
+
+
 class Spaceship(All_objects):
-    
+
     def __init__(self, x=None, y=None, x_speed=None, y_speed=None, rotation=None):
-        
-        self.keys=set()
-        super().__init__("obrazkyAST/PNG/raketa1.png", x=window.width/2, y=77)
-        self.x_speed=0
-        self.y_speed=0
-        self.rotation=0
-        self.rozmer = min(self.image.width, self.image.height)/2
-        
-    def tick(self,dt):
+
+        self.keys = set()
+        super().__init__(
+            "obrazkyAST/PNG/raketa1.png", x=window.width / 2, y=77)
+        self.x_speed = 0
+        self.y_speed = 0
+        self.rotation = 0
+        self.rozmer = min(self.image.width, self.image.height) / 2
+
+    def tick(self, dt):
         for sym in self.keys:
-            if sym==key.RIGHT:
-                self.rotation=self.rotation + 10
-            elif sym==key.LEFT:
-                self.rotation=self.rotation - 10    
-            elif sym==key.UP:
+            if sym == key.RIGHT:
+                self.rotation = self.rotation + 10
+            elif sym == key.LEFT:
+                self.rotation = self.rotation - 10
+            elif sym == key.UP:
                 self.x_speed = 350
                 self.y_speed = 350
-                self.x=self.x + dt * self.x_speed*cos(pi/2 - radians(self.rotation))
-                self.y=self.y + dt * self.y_speed*sin(pi/2 - radians(self.rotation))
-            elif sym==key.DOWN:
-                self.x_speed=200
-                self.y_speed=200
-                self.x=self.x + dt * self.x_speed* (-cos(pi/2 - radians(self.rotation)))
-                self.y=self.y + dt * self.y_speed*(-sin(pi/2 - radians(self.rotation)))
-        
+                self.x = self.x + dt * self.x_speed * \
+                    cos(pi / 2 - radians(self.rotation))
+                self.y = self.y + dt * self.y_speed * \
+                    sin(pi / 2 - radians(self.rotation))
+            elif sym == key.DOWN:
+                self.x_speed = 200
+                self.y_speed = 200
+                self.x = self.x + dt * self.x_speed * \
+                    (-cos(pi / 2 - radians(self.rotation)))
+                self.y = self.y + dt * self.y_speed * \
+                    (-sin(pi / 2 - radians(self.rotation)))
+
+
 class Meteor(All_objects):
-    
-    def __init__(self, x=None, y=None, img_file=None, direction=None,speed=None, rspeed=None, rozmer=None):
-        
+
+    def __init__(self, x=None, y=None, img_file=None, direction=None, speed=None, rspeed=None, rozmer=None):
+
         if img_file is None:
-            num=random.choice(range(1,20))
-            img_file=("obrazkyAST/PNG/Meteors/{}.png".format(num))
-        super().__init__(img_file, x, y=window.height+20)
-        
-        self.direction=direction if direction is not None else random.randint(150,220)
-        self.speed=speed if speed is not None else random.randint(100,400)
-        self.rspeed=rspeed if rspeed is not None else random.randint(-50,50)
-        self.rozmer=min(self.image.width,self.image.height)/2
-        
+            num = random.choice(range(1, 20))
+            img_file = ("obrazkyAST/PNG/Meteors/{}.png".format(num))
+        super().__init__(img_file, x, y=window.height + 20)
+
+        self.direction = direction if direction is not None else random.randint(
+            150, 220)
+        self.speed = speed if speed is not None else random.randint(100, 400)
+        self.rspeed = rspeed if rspeed is not None else random.randint(-50, 50)
+        self.rozmer = min(self.image.width, self.image.height) / 2
+
     def tick(self, dt):
-        self.x=self.x + dt * self.speed * cos(pi / 2 - radians(self.direction))
-        self.y=self.y + dt * self.speed * sin(pi / 2 - radians(self.direction))
-        self.rotatin= self.rotation + dt * self.rspeed
-        
-        if self.x + self.rozmer> window.width + 200:
+        self.x = self.x + dt * self.speed * \
+            cos(pi / 2 - radians(self.direction))
+        self.y = self.y + dt * self.speed * \
+            sin(pi / 2 - radians(self.direction))
+        self.rotatin = self.rotation + dt * self.rspeed
+
+        if self.x + self.rozmer > window.width + 200:
             actions.meteors.remove(self)
         elif self.x + self.rozmer < -200:
             actions.meteors.remove(self)
         elif self.y + self.rozmer < -200:
             actions.meteors.remove(self)
-            
+
     def __del__(self):
         print(id(self))
-        
+
+
 class Laser(All_objects):
 
     def __init__(self, img_file=None, speed=None, direction=None):
-        
-        super().__init__("obrazkyAST/PNG/Effects/fire01.png", x=ship.x, y=ship.y+70, anchor_y=self.height)
-        
-        self.direction=0
-        self.speed=1000
-        
+
+        super().__init__("obrazkyAST/PNG/Effects/fire01.png",
+                         x=ship.x, y=ship.y + 70)
+        self.anchor_y=self.height
+        self.rotation = 90
+        self.speed = 1000
+
     def tick(self, dt):
         self.y = self.y + dt * self.speed
-        for sym in ship.keys:
-            if sym == key.SPACE:
-                actions.add_laser()
-        
+        # for sym in ship.keys:
+        #     if sym == key.SPACE:
+        #         actions.add_laser()
+
         if self.y > window.height + 200:
             actions.lasers.remove(self)
 
+
 class Actions():
 
-    meteors=[]
-    lasers=[]
-    
-    def add_meteor(self,dt=None):
+    meteors = []
+    lasers = []
+
+    def add_meteor(self, dt=None):
         self.meteors.append(Meteor())
-        
+
     def add_laser(self, dt=None):
-        self.lasers.append(Laser())    
-              
+        self.lasers.append(Laser())
+
     def tick(self, dt):
         # pohnu kamenama
         for meteor in self.meteors:
             meteor.tick(dt)
             distance = ((meteor.x - ship.x)**2 + (meteor.y - ship.y)**2)**0.5
-            if distance - meteor.rozmer/2 -45 <=0:
+            if distance - meteor.rozmer / 2 - 45 <= 0:
                 self.colision()
-                
+
             for laser in self.lasers:
-                if laser.y >= meteor.y and laser.x >= (meteor.x - meteor.rozmer/3) and laser.x <= (meteor.x + meteor.rozmer/3):
+                if laser.y >= meteor.y and laser.x >= (meteor.x - meteor.rozmer / 3) and laser.x <= (meteor.x + meteor.rozmer / 3):
                     self.lasers.remove(laser)
                     self.meteors.remove(meteor)
-                    
+
         for laser in self.lasers:
             laser.tick(dt)
-                
+
     def colision(self):
         pyglet.clock.unschedule(ticky)
-        pyglet.clock.unschedule(actions.add_meteor)  
-        ship.x_speed=0
-        ship.y_speed=0
-        print("Prohrál jsi")          
-        
+        pyglet.clock.unschedule(actions.add_meteor)
+        ship.x_speed = 0
+        ship.y_speed = 0
+        print("Prohrál jsi")
+
+
 def ticky(dt):
     actions.tick(dt)
     ship.tick(dt)
-            
+
+
 @window.event
 def on_key_press(sym, mod):
     ship.keys.add(sym)
-#    if sym == key.SPACE:
-#        actions.add_laser()
+    if sym == key.SPACE:
+        actions.add_laser()
+
 
 @window.event
 def on_key_release(sym, mod):
     ship.keys.remove(sym)
+
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
 
-ship=Spaceship()
-actions=Actions()
+ship = Spaceship()
+actions = Actions()
 actions.add_meteor()
 actions.add_meteor()
-pyglet.clock.schedule_interval(ticky, 1/30)
-pyglet.clock.schedule_interval(actions.add_meteor, 10/3)
+pyglet.clock.schedule_interval(ticky, 1 / 30)
+pyglet.clock.schedule_interval(actions.add_meteor, 10 / 3)
 pyglet.app.run()
